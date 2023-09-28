a24b935ffa21d408dcbcb97495fadfbecf0d2912
Kanyinsola Saheed <kanyinsola.saheed.ec@crowdbotics.com>
2023-09-28 15:44:43 +0100

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,5 +1,3 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "dark-wave-43082",
   url: "https://dark-wave-43082.botics.co",
@@ -7,9 +5,9 @@ export const globalOptions = {
   screenOptions: {
     headerShown: false
   }
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
