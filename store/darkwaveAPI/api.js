e638dfc1c3d6f059e9b2719da4587d912d4ff6fe
Crowdbotics <engineering@crowdbotics.com>
2023-10-02 22:34:26 +0000

    Update store.

--- store/darkwaveAPI/api.js
+++ store/darkwaveAPI/api.js
@@ -9,13 +9,13 @@ function api_docs_schema_retrieve(payload) {
   })
 }
 function api_v1_login_create(payload) {
-  return darkwaveAPI.post(`/api/v1/login/`, payload.data)
+  return darkwaveAPI.post(`/api/v1/login/`, payload)
 }
 function api_v1_signup_create(payload) {
-  return darkwaveAPI.post(`/api/v1/signup/`, payload.data)
+  return darkwaveAPI.post(`/api/v1/signup/`, payload)
 }
 function rest_auth_login_create(payload) {
-  return darkwaveAPI.post(`/rest-auth/login/`, payload.data)
+  return darkwaveAPI.post(`/rest-auth/login/`, payload)
 }
 function rest_auth_logout_retrieve(payload) {
   return darkwaveAPI.get(`/rest-auth/logout/`)
@@ -24,28 +24,28 @@ function rest_auth_logout_create(payload) {
   return darkwaveAPI.post(`/rest-auth/logout/`)
 }
 function rest_auth_password_change_create(payload) {
-  return darkwaveAPI.post(`/rest-auth/password/change/`, payload.data)
+  return darkwaveAPI.post(`/rest-auth/password/change/`, payload)
 }
 function rest_auth_password_reset_create(payload) {
-  return darkwaveAPI.post(`/rest-auth/password/reset/`, payload.data)
+  return darkwaveAPI.post(`/rest-auth/password/reset/`, payload)
 }
 function rest_auth_password_reset_confirm_create(payload) {
-  return darkwaveAPI.post(`/rest-auth/password/reset/confirm/`, payload.data)
+  return darkwaveAPI.post(`/rest-auth/password/reset/confirm/`, payload)
 }
 function rest_auth_registration_create(payload) {
-  return darkwaveAPI.post(`/rest-auth/registration/`, payload.data)
+  return darkwaveAPI.post(`/rest-auth/registration/`, payload)
 }
 function rest_auth_registration_verify_email_create(payload) {
-  return darkwaveAPI.post(`/rest-auth/registration/verify-email/`, payload.data)
+  return darkwaveAPI.post(`/rest-auth/registration/verify-email/`, payload)
 }
 function rest_auth_user_retrieve(payload) {
   return darkwaveAPI.get(`/rest-auth/user/`)
 }
 function rest_auth_user_update(payload) {
-  return darkwaveAPI.put(`/rest-auth/user/`, payload.data)
+  return darkwaveAPI.put(`/rest-auth/user/`, payload)
 }
 function rest_auth_user_partial_update(payload) {
-  return darkwaveAPI.patch(`/rest-auth/user/`, payload.data)
+  return darkwaveAPI.patch(`/rest-auth/user/`, payload)
 }
 export const apiService = {
   api_docs_schema_retrieve,
